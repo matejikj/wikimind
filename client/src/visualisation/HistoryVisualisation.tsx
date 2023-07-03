a47a5cff9267478925d6e4321c092b931952d793
Jakub Matejik <matejik@ttc.cz>
2023-07-03 18:29:53 +0200

    carousel

--- client/src/visualisation/HistoryVisualisation.tsx
+++ client/src/visualisation/HistoryVisualisation.tsx
@@ -3,7 +3,7 @@ import Sidenav from "../components/Sidenav";
 import { SessionContext } from "../sessionContext";
 import { useLocation, useNavigate } from "react-router-dom";
 import { DateGroup, aaa, bbbb, randomDates } from "../pages/utils";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Carousel, Col, Container, Form, Row } from "react-bootstrap";
 import { ChoiceSelection, groupDates } from "./utiils";
 import { ResultItem } from "../models/ResultItem";
 import { MindMapDataset } from "../models/types/MindMapDataset";
@@ -102,12 +102,53 @@ const HistoryVisualisation: React.FC<{
               {key && dateGroups[key] && dateGroups[key].map((item) => {
                 return (
                   <div>
-                   {item.label.value + ": " + item.propertyLabel.value + " - " + item.value.value}
+                    {item.label.value + ": " + item.propertyLabel.value + " - " + item.value.value}
                   </div>
-                  )
+                )
               })}
             </p>
           </Col>
+        </Row>
+        <Row>
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://www.gstatic.com/images/icons/material/apps/fonts/1x/catalog/v5/opengraph_color.png"
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://assets.awwwards.com/awards/element/2022/11/637e0b3a0dfa5353136855.jpg"
+                alt="Second slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://assets.awwwards.com/awards/images/2021/08/google-fonts-cover-1.jpg"
+                alt="Third slide"
+              />
+
+              <Carousel.Caption>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
         </Row>
       </Container >
     )
